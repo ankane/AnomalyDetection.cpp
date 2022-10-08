d9a48db1a2a974d5cf03694204beeb19420946da
Andrew Kane <andrew@ankane.org>
2022-10-08 16:28:57 -0700

    Added example

---
+++ example/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "anomaly_detection.hpp"
+
+int main() {
+    std::vector<float> series = {
+        5.0, 9.0, 2.0, 9.0, 0.0, 6.0, 3.0, 8.0, 5.0, 18.0,
+        7.0, 8.0, 8.0, 0.0, 2.0, 15.0, 0.0, 5.0, 6.0, 7.0,
+        3.0, 6.0, 1.0, 4.0, 4.0, 4.0, 30.0, 7.0, 5.0, 8.0
+    };
+    auto period = 7; // number of observations in a single period
+
+    auto res = anomaly_detection::params().fit(series, period);
+    for (auto anomaly : res.anomalies) {
+        std::cout << anomaly << std::endl;
+    }
+
+    return 0;
+}
